7b790a595c9c4292e4412ea0e304a02cead29ea2
David Annebicque <david.annebicque@gmail.com>
2022-01-27 16:57:16 +0100

    seance 1

---
+++ src/Controller/LuckyController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Controller;
+
+use DateTime;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LuckyController extends AbstractController
+{
+    /**
+     * @Route("/lucky/number", name="app_lucky_number")
+     */
+    public function number()
+    {
+        $number = random_int(0, 100);
+
+        return $this->render(
+            'lucky/number.html.twig',
+            [
+                'number' => $number
+            ]
+        );
+    }
+
+    /**
+     * @Route("/time/now", name="app_time_now")
+     */
+    public function timeNow()
+    {
+        $date = new DateTime();
+
+        return $this->render(
+            'lucky/time.html.twig',
+            [
+                'date' => $date
+            ]
+        );
+    }
+
+    /**
+     * @Route("/color/{color}", name="app_color")
+     */
+    public function color($color)
+    {
+
+        return $this->render(
+            'lucky/color.html.twig',
+            [
+                'couleur' => $color
+            ]
+        );
+    }
+
+    /**
+     * @Route("/request", name="app_request")
+     */
+    public function mesinfos(Request $request)
+    {
+        return $this->render('lucky/request.html.twig', [
+            'request' => $request
+        ]);
+    }
+}
